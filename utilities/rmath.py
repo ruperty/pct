d1bd3221b4fcdb1c9e0fd35b93323b02cfa0e892
rupert <rupert@moonsit.co.uk>
2020-06-27 11:03:27 +0100

    u

--- utilities/rmath.py
+++ utilities/rmath.py
@@ -15,6 +15,29 @@ def smooth( newVal, oldVal, weight) :
 def sigmoid(x, range, scale) :
     return -range / 2 + range / (1 + math.exp(-x * scale / range));
     
+
+
+class Counter(object):
+
+  def __init__(self, limit, init=0, step=1):
+      self.limit=limit
+      self.counter=init
+      self.step=step
+
+  def __call__(self):
+      self.counter+=self.step
+  
+  def get(self):
+      return self.counter
+    
+  def get_limit(self):
+      return self.limit
+
+  def set_limit(self, limit):
+      self.limit=limit
+
+
+
 """
 rang=0.4
 scale=.5
