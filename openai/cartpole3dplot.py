6ce57029d1d75465bedd1bf4cd82b4d0ef2730a9
Rupert Young <rupertjyoung@gmail.com>
2020-07-14 20:34:16 +0100

    u

--- openai/cartpole3dplot.py
+++ openai/cartpole3dplot.py
@@ -12,71 +12,9 @@ import numpy
 import pct.utilities.timing as tim
 from pct.dl.models.cartpole import CartpoleTuning
 
-live_plot = False
-offline_plot=False#True#False
-training=False#True
-render_gym=False#True
-batch_size=10
-num_runs=1000
-prnt=False
-
-order = {"value1":0, "value2":1}
-
-pa=[]
-pv=[]
-cp=[]
-list1=pa
-list2=pv
 
 
-# weights=[1.6,       1,       1,       -0.5]
-
-test =1
-
-if test ==1: # all
-    step=1
-    scale=10
-
-    pa_start= 0
-    pa_stop= 8.0
-
-    pv_start= 0.5
-    pv_stop=2.0
-
-if test ==2: # single
-    step=1
-    scale=10
-
-    pa_start= -1.0
-    pa_stop= 10.0
-
-    pv_start= -1
-    pv_stop=2.0
-
-
-pa_ss = '[[{:-04.1f}][{:-04.1f}]'.format(pa_start,pa_stop)
-pv_ss = '[{:-04.1f}][{:-04.1f}]]'.format(pv_start,pv_stop)
-#print(pv_ss)
-
-
-for pa_wt in range(int(pa_start* scale), int(pa_stop* scale), step):
-    pa.append(pa_wt/scale)
-
-for pv_wt in range(int(pv_start* scale), int(pv_stop* scale), step):
-    pv.append(pv_wt/scale)
-
-
-#for cp_wt in range(2, 30, 1):
-#    cp.append(cp_wt/scale)
-
-
-
-#print(pa)        
-#print(pv)        
-#print(cp)        
-
-
-def run_cartpole(ctr, weights):
+def run_cartpole(ctr, weights, num_runs, prnt, live_plot, offline_plot, render_gym, batch_size, training, min_runs):
     timer = tim.Timer()
     timer.start()
     ct = CartpoleTuning("Cartpole", "CartPole-v1", num_runs+1, [True, True, True, False], print= prnt)
@@ -85,7 +23,7 @@ def run_cartpole(ctr, weights):
     
     wts=ct.run( batch_size, training)        
     actual_runs= ct.counter.get()
-    if actual_runs<200:
+    if actual_runs<min_runs:
         mean =0 
     else: 
         mean = ct.data.get_mean().numpy()
@@ -100,7 +38,7 @@ def run_cartpole(ctr, weights):
     return mean      
 
 
-def run_batch(list1, list2, order, weights, scale):
+def run_batch(list1, list2, order, weights, scale, num_runs, prnt, live_plot, offline_plot, render_gym, batch_size, training, min_runs):
     ctr = len(list1)*len(list2)
     losses=[]
     for value2 in list2:
@@ -108,7 +46,7 @@ def run_batch(list1, list2, order, weights, scale):
         inner_losses=[]
         for value1 in list1:
             weights[order["value1"]]=value1/scale
-            loss=run_cartpole(ctr, weights)
+            loss=run_cartpole(ctr, weights, num_runs, prnt, live_plot, offline_plot, render_gym, batch_size, training, min_runs)
             inner_losses.append(loss)    
             ctr-=1
         losses.append(inner_losses)
@@ -118,10 +56,10 @@ def run_batch(list1, list2, order, weights, scale):
 
    
 
-def run_data(list1,list2,type,pa_ss, pv_ss,other_wt_name,other_wt,other_wt_index, order):
+def run_data(list1,list2,type,pa_ss, pv_ss,other_wt_name,other_wt,other_wt_index, order, num_runs, prnt, live_plot, offline_plot, render_gym, batch_size, training, min_runs):
     weights=[0,       0,       0,       -0.05]
     weights[other_wt_index]=other_wt
-    losses=run_batch(list1, list2, order, weights, 1)
+    losses=run_batch(list1, list2, order, weights, 1,  num_runs, prnt, live_plot, offline_plot, render_gym, batch_size, training, min_runs)
     #print(list1)
     #print(list2)
     #print(losses)
@@ -146,46 +84,19 @@ def get_filename(type,pa_ss, pv_ss, other_wt_name,other_wt):
 
 
 
-def run_multiple(pa_ss, pv_ss,other_wt):
+def run_multiple(list1,list2,type,pa_ss, pv_ss,
+    other_wt_name,other_wt,other_wt_index, order, num_runs, 
+    prnt, live_plot, offline_plot, render_gym, batch_size, 
+    training, min_runs):
     timer = tim.Timer()
     timer.start()
-    run_data(list1,list2,type,pa_ss, pv_ss,other_wt_name,other_wt,other_wt_index, order)
+    run_data(list1,list2,type,pa_ss, pv_ss,
+    other_wt_name,other_wt,other_wt_index,order, num_runs, 
+    prnt, live_plot, offline_plot, render_gym, batch_size, 
+    training, min_runs)
     print("Time %0.3f points %d runs %d" % ( timer.stop(), len(list1)*len(list2), num_runs))
 
 
-
-
-type="papv"
-other_wt_name="cp"
-other_wt_index=2
-
-"""
-filen = get_filename(type,pa_ss, pv_ss, other_wt_name,1.5)
-print(filen)
-x = [1]
-numpy.save(filen, x)
-
-"""
-
-
-
-if test ==1: # all
-    cp_start=0.8
-    cp_stop=1.6
-    cp_step=1
-    cp_scale=10
-
-
-if test ==2: # single
-    cp_start=1
-    cp_stop=1.1
-    cp_step=1
-    cp_scale=10
-
-for wt in range(int(cp_start*cp_scale), int(cp_stop*cp_scale), cp_step):
-        run_multiple(pa_ss, pv_ss,wt/cp_scale)
-
-    
 
  
 
